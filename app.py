687a281392cb4a1f26b587f942385f005325d61b
Santos Flores <santosflores@gmail.com>
2024-08-19 22:44:51 +0000

    Milestone 3

--- app.py
+++ app.py
@@ -8,24 +8,26 @@ endpoint_url = "https://api.openai.com/v1"
 client = openai.AsyncClient(api_key=api_key, base_url=endpoint_url)
 
 # https://platform.openai.com/docs/models/gpt-4o
-model_kwargs = {
-    "model": "chatgpt-4o-latest",
-    "temperature": 0.3,
-    "max_tokens": 500
-}
+model_kwargs = {"model": "chatgpt-4o-latest", "temperature": 0.3, "max_tokens": 500}
+
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    # Your custom logic goes here...
+    message_history = cl.user_session.get("message_history", [])
+    message_history.append({"role": "user", "content": message.content})
 
-    response = await client.chat.completions.create(
-        messages=[{"role": "user", "content": message.content}],
-        **model_kwargs
+    response_message = cl.Message(content="")
+    await response_message.send()
+
+    stream = await client.chat.completions.create(
+        messages=message_history, stream=True, **model_kwargs
     )
 
-    # https://platform.openai.com/docs/guides/chat-completions/response-format
-    response_content = response.choices[0].message.content
+    async for part in stream:
+        if token := part.choices[0].delta.content or "":
+            await response_message.stream_token(token)
 
-    await cl.Message(
-        content=response_content,
-    ).send()
+    await response_message.update()
+
+    message_history.append({"role": "assistant", "content": response_message.content})
+    cl.user_session.set("message_history", message_history)
